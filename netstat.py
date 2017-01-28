e7b3117bf3f91466883a0e7110496ccf3874950a
petel1306 <oripetel1306@gmail.com>
2017-01-28 13:49:37 +0200

    Add first part parsing

--- netstat.py
+++ netstat.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python
+import os
+import glob
+
+def trans_ipv4(hex_address):
+    '''parse ip address and port'''
+    hex_ip, hex_port = hex_address.split(':')
+    parts = (htod(hex_port), htod(hex_ip[:2]), htod(hex_ip[2:4]), htod(hex_ip[4:6]), htod(hex_ip[6:8]))
+    return '{}.{}.{}.{}:{}'.format(*parts[::-1])
+
+def htod(num):
+    '''from hex to decimal'''
+    return int(num, 16)
