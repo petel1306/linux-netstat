93db9a42e374824d338861188256f2067444d854
petel1306 <oripetel1306@gmail.com>
2017-01-28 14:50:17 +0200

    Add socket state parsing

--- netstat.py
+++ netstat.py
@@ -2,12 +2,28 @@
 import os
 import glob
 
+def htod(num):
+    '''from hex to decimal'''
+    return int(num, 16)
+
 def trans_ipv4(hex_address):
     '''parse ip address and port'''
     hex_ip, hex_port = hex_address.split(':')
     parts = (htod(hex_port), htod(hex_ip[:2]), htod(hex_ip[2:4]), htod(hex_ip[4:6]), htod(hex_ip[6:8]))
     return '{}.{}.{}.{}:{}'.format(*parts[::-1])
 
-def htod(num):
-    '''from hex to decimal'''
-    return int(num, 16)
+def trans_state(is_udp, state):
+    '''returns the socket state, if the socket is tcp'''
+    return '-' if is_udp else {
+        '01': 'ESTABLISHED',
+        '02': 'SYN_SENT',
+        '03': 'SYN_RECV',
+        '04': 'FIN_WAIT1',
+        '05': 'FIN_WAIT2',
+        '06': 'TIME_WAIT',
+        '07': 'CLOSE',
+        '08': 'CLOSE_WAIT',
+        '09': 'LAST_ACK',
+        '0A': 'LISTEN',
+        '0B': 'CLOSING'
+    }[state]
