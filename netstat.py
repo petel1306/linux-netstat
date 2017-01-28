18c3512a90d88657ff999ea51867bf4a367a9dc4
petel1306 <oripetel1306@gmail.com>
2017-01-28 17:09:55 +0200

    Add pid finder

--- netstat.py
+++ netstat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import glob
+import re
 
 def htod(num):
     '''from hex to decimal'''
@@ -41,3 +42,13 @@ def trans_timer(timer):
     }[kind]
     time = htod(time)/100.0
     return '{} ({})'.format(kind, time)
+
+def find_pid(inode):
+    '''finds the pid of the process which has the requested inode'''
+    for fd in glob.glob('/proc/[0-9]*/fd/[0-9]*'):
+        try:
+            if re.search(inode, os.readlink(fd)):
+                return fd.split('/')[2]
+        except:
+            pass
+    return '-'
